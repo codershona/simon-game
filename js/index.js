e4744bc8656b51506b6ee3d7f92aba15cee6ded6
codershona <falgunimst95@gmail.com>
2020-08-16 15:19:05 +0200

    working with start button

--- js/index.js
+++ js/index.js
@@ -36,8 +36,87 @@ const onButton = document.querySelector("#on");
 
 const startButton = document.querySelector("#start");
 
+strictButton.addEventListener('click', (event) => {
+
+	// console.log("checked");
+
+	if (strictButton.checked === true) {
+
+		strict = true;
+	} else {
+
+
+		strict = false;
+
+	}
+
+
+})
 
 
 
+onButton.addEventListener('click', (event) => {
+
+	if (onButton.checked == true) {
+
+		on = true;
+
+		turnCounter.innerHTML = "-";
 
 
+	} else {
+
+		on = false;
+
+		turnCounter.innerHTML = "";
+
+		clearColor();
+
+		clearInterval(intervalId);
+
+
+	}
+
+
+})
+
+
+startButton.addEventListener('click', (event) => {
+
+	if (on || win) {
+
+		play();
+
+	}
+
+})
+
+
+
+function play() {
+
+	win = false;
+	order = [];
+	playerOrder = [];
+
+	flash = 0;
+	intervalid = 0;
+
+   turn = 1;
+
+   turnCounter.innerHTML = 1;
+
+   good = true;
+
+   for (var i = 0; i < 20; i++) {
+
+   	order.push(Math.floor(Math.random() * 4) + 1);
+
+
+
+   }
+
+   console.log(order);
+
+
+}
